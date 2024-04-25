9aa794bf5038217705da3d609df3c872a64ee58d
Teamon9161 <1127972945@qq.com>
2024-04-25 13:28:06 +0800

    update pyo3 pyo3-polars and polars

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ mod rolling_rank;
 mod strategy;
 
 use pyo3::types::PyModule;
-use pyo3::{pymodule, PyResult, Python};
+use pyo3::{pymodule, Bound, PyResult, Python};
 
 #[pymodule]
-fn polars_qt(_py: Python, m: &PyModule) -> PyResult<()> {
+fn polars_qt(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
